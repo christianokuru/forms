518070160b0769522eaf962aad7ad916748393c0
christianokuru <okuruchristian@gmail.com>
2024-10-10 19:30:47 +0100

    finished sign in page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,8 @@
+import SignUpForm from "@/app/components/form/SignUpForm";
 
 
 export default function signUp() {
   return (
-    <div className="bg-blue-500 w-full">hiii</div>
+    <div className="w-full"><SignUpForm /></div>
   );
 }
